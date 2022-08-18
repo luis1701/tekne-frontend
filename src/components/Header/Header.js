6c2010a631f937e03d15d340944e13a65e54366a
juan512 <juandanielpcutile.jc@gmail.com>
2022-08-17 21:57:57 -0400

    Logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Button from '../Button/Button';
 
 import './Header.css';
 
 const Header = props => {
+
+  const clearCacheData = () => {
+    localStorage.clear();
+    window.location.reload(true)
+  };
+  
   return (
     <header className="header">
       <h1>MERN Shop</h1>
+      <div>
+        <Button onClick={() => clearCacheData()}>Logout</Button>  
+      </div>
     </header>
   );
 };
